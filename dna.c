ed7f560bd0cdcd399376d6b63017761b0eb413fb
Lucastakeshi <48793578+lucastakashi@users.noreply.github.com>
2019-06-27 10:09:36 -0300

    Create dna.c

---
+++ dna.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+void main(){
+char fita [10];
+char *f;
+int coluna [2][10];
+f=fita;
+printf("Por favor informe nucletideos: \n");
+scanf("%s", fita);
+printf("\n\t\tO o nucletideo digitado é: %s ",fita);
+
+}
